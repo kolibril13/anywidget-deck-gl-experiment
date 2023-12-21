aac39869e5dbeacae8367ebf5b4d036729ea45a5
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-12-21 12:32:27 +0100

    minimal example

--- js/widget.jsx
+++ js/widget.jsx
@@ -1,15 +1,39 @@
 import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
+import DeckGL from "@deck.gl/react";
+import { LineLayer } from "@deck.gl/layers";
 import "./widget.css";
 
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0,
+};
+
+const data = [
+  {
+    sourcePosition: [-122.41669, 37.7853],
+    targetPosition: [-122.41669, 37.781],
+  },
+];
+
 export const render = createRender(() => {
-	const [value, setValue] = useModelState("value");
-	return (
-		<button
-			className="anywidget_deck_gl_experiment-counter-button"
-			onClick={() => setValue(value + 1)}
-		>
-			count is {value}
-		</button>
-	);
+  const [value, setValue] = useModelState("value");
+  const layers = [new LineLayer({ id: "line-layer", data })];
+
+  return (
+    <div style={{position:"relative", width: "500px", height: "500px" }}>
+      <h1>hello</h1>
+	  <DeckGL
+       initialViewState={INITIAL_VIEW_STATE}
+       controller={true}
+       layers={layers}
+     />
+    </div>
+  );
 });
+
+
+
